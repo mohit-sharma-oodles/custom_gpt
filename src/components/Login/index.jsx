7e782aac1bec7fc6f0af331c439fae6a26274369
Mohit <mohit.sharma1@oodles.io>
2024-10-05 00:29:41 +0530

    api added to google login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -95,7 +95,7 @@ const Login = ({ isOpen, onClose, onSignupClick }) => {
   }, [status, isAuthenticated, navigate, onClose]);
 
   const handleGoogleLogin = async () => {
-    const googleAuthURL = "https://customgpt-b.chattodata.com/auth/google/";
+    const googleAuthURL = "https://customgpt-b.chattodata.com/api/auth/google/";
     window.location.href = googleAuthURL;
 
     // const popupListener = setInterval(() => {
