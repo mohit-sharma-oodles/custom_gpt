6a52535be67db24d75b908ead834fdb56b381979
Mohit <mohit.sharma1@oodles.io>
2024-11-15 12:11:55 +0530

    changed prod domain

--- src/Axios/axiosInstance.js
+++ src/Axios/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import updateToken from "./utils";
 
-// const domain = "https://749d-125-63-73-50.ngrok-free.app";
+// const domain = "https://820e-14-102-190-50.ngrok-free.app";
 // const domain = "https://customgpt-b.chattodata.com";
-const domain = "https://customgpt-b.chattodata.com";
+const domain = "https://customgpt-b.oodleslab.com";
 export const baseURL = `${domain}`;
 
 export const axios_instance = axios.create({
