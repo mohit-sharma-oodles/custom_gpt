6adae04f23a44331ceed5ed133c83147b00a615e
Mohit <mohit.sharma1@oodles.io>
2025-01-13 09:53:23 +0530

    oodleslab url

--- src/Axios/axiosInstance.js
+++ src/Axios/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import updateToken from "./utils";
 
 // const domain = "https://766b-14-102-190-50.ngrok-free.app";
-const domain = "https://customgpt-b.chattodata.com";
-// const domain = "https://customgpt-b.oodleslab.com";
+// const domain = "https://customgpt-b.chattodata.com";
+const domain = "https://customgpt-b.oodleslab.com";
 export const baseURL = `${domain}`;
 
 export const axios_instance = axios.create({
